414fc26070c69d3f09d5f878b7660312f0bb1304
rohit1101 <srohit1101@gmail.com>
2020-12-01 11:26:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import {
+  Container,
+  createMuiTheme,
+  makeStyles,
+  Paper,
+  ThemeProvider,
+  Typography,
+} from "@material-ui/core"
 import React from "react"
 
+const useStyles = makeStyles({})
+
 function App() {
-  return
+  const theme = createMuiTheme({
+    palette: {
+      type: "dark",
+    },
+  })
+
+  const classes = useStyles({
+    root: {
+      background: "black",
+      overflow: "hidden",
+    },
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper className={classes.root}>
+        <Typography>Hello</Typography>
+      </Paper>
+    </ThemeProvider>
+  )
 }
 
 export default App
