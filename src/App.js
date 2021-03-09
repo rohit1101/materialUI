3fe03ef3c78d71866690be31af5c11f6c2a912a3
rohit1101 <srohit1101@gmail.com>
2021-03-09 15:39:00 +0530

    Final Commit: Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
 import {
   AppBar,
+  Box,
   Button,
   Container,
+  createMuiTheme,
+  Divider,
   IconButton,
+  Paper,
+  ThemeProvider,
   Toolbar,
+  Typography,
 } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
+
+const whiteTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#e3f2fd",
+    },
+    secondary: {
+      main: "#f48fb1",
+    },
+  },
+});
 
 export default function App() {
   return (
-    <>
-      <Container>
-        <AppBar color="default">
-          <Toolbar>
-            <IconButton></IconButton>
-            <Button color="primary">Subscribe</Button>
-          </Toolbar>
-        </AppBar>
-      </Container>
-    </>
+    <ThemeProvider theme={whiteTheme}>
+      <Paper style={{ backgroundColor: "black" }} square={true}>
+        <Container maxWidth="lg">
+          <Paper
+            style={{ height: "100vh", backgroundColor: "black" }}
+            square={true}
+          >
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              padding={1}
+            >
+              <Button
+                size="small"
+                color="primary"
+                variant="outlined"
+                style={{ fontWeight: "600" }}
+              >
+                Subscribe
+              </Button>
+              <Typography variant="h5" color="primary">
+                Blog
+              </Typography>
+              <Box>
+                <IconButton color="primary">
+                  <SearchOutlined />
+                </IconButton>
+                <Button size="small" color="primary" variant="outlined">
+                  Sign Up
+                </Button>
+              </Box>
+            </Box>
+            <Divider style={{ backgroundColor: "#2E2E30" }} />
+          </Paper>
+        </Container>
+      </Paper>
+    </ThemeProvider>
   );
 }
